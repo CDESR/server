6958024ae38033c29839648e1bbccf66715b9bbb
Rencheng Chua <rench00@gmail.com>
2016-08-12 13:35:41 +0800

    try bootstrap

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT) || 7000);
 
 app.listen(app.get('port'), function(){
-  console.log('My express server is running at localhost', app.get('port'))
+  console.log('My express server is running at localhost', app.get('port'));
 });
 
 module.exports = app;
